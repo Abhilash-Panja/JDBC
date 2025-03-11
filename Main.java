a2a17f1fe761b7370de0c2777c61608001947943
Abhilash Panja <abhilashpanja7@gmail.com>
2025-03-11 17:32:26 +0530

    Using the createStatement

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 class Main{
-  private static final String URL="jdbc:mysql://localhost:3306/?user=root";
+  private static final String URL="jdbc:mysql://localhost:3306/student";
   private static final String USER="root";
   private static final String PASS="root";
   public static void main(String[] args) {
@@ -15,6 +16,16 @@ class Main{
     }
     try{
       Connection connection=DriverManager.getConnection(URL, USER, PASS);
+      Statement statement=(Statement)connection.createStatement();
+      String Query="select * from student_table";
+      ResultSet resultSet=((java.sql.Statement) statement).executeQuery(Query);
+      while (resultSet.next()) {
+        int id=resultSet.getInt("id");
+        int age=resultSet.getInt("age");
+        String name=resultSet.getString("name");
+        double marks=resultSet.getDouble("marks");
+        System.out.println("id"+" "+id+" "+"age"+" "+age+" "+"name"+" "+name+" "+"marks"+" "+marks);
+      }
     }
     catch(SQLException e){
       System.out.println(e.getMessage());
