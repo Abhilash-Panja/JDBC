bb4dcbd87ab90f14eeb405710d9fb688f2fcb09f
Abhilash Panja <abhilashpanja7@gmail.com>
2025-03-12 17:29:44 +0530

    Created a boiler plate code for Connecting the Databse using JDBC and using createStatement

--- Main.java
+++ Main.java
@@ -8,6 +8,11 @@ class Main{
   private static final String USER="root";
   private static final String PASS="root";
   public static void main(String[] args) {
+    /*****************************************************************************************
+     
+    Step-1: Loading the Driver's by using Class.forName()
+    
+    *****************************************************************************************/
     try{
     Class.forName("com.mysql.cj.jdbc .Diver");
     }
@@ -15,19 +20,53 @@ class Main{
       System.out.println(e.getMessage());
     }
     try{
+      /*****************************************************************************************
+      
+      Step-2: Creating a Connection object by using DriverManager and
+      we make use of the connection interface to store the instance of the 
+      connection created by the DriverManager;
+
+     *****************************************************************************************/
       Connection connection=DriverManager.getConnection(URL, USER, PASS);
-      Statement statement=(Statement)connection.createStatement();
-      String insertQuery = "INSERT INTO student_table (id, name, marks, age) VALUES (3, 'Priyanshu', 90.89, 20)";
-      String selectQuery = "SELECT * FROM student_table";
-      statement.executeUpdate(insertQuery);
-      ResultSet resultSet = statement.executeQuery(selectQuery);
-      while (resultSet.next()) {
-        int id=resultSet.getInt("id");
-        int age=resultSet.getInt("age");
-        String name=resultSet.getString("name");
-        double marks=resultSet.getDouble("marks");
-        System.out.println("id"+" "+id+" "+"age"+" "+age+" "+"name"+" "+name+" "+"marks"+" "+marks);
-      }
+      /*****************************************************************************************
+      
+      Step-3: Creating a Statement object by using the Connection interface 
+      to excute the SQL Queries.       
+     
+      *****************************************************************************************/
+      Statement statement=connection.createStatement();
+      /*****************************************************************************************
+       
+      Step-4: Executing the SQL Queries by using the Statement interface
+       And  when we are using the createStatement() we have to write the SQL
+       Commands Manually.
+       And we have Different types of SQL Commands like
+       1. SELECT
+       2. INSERT
+       3. UPDATE
+       4. DELETE
+       To excute the "SELECT" we will make use of the statement.executeQuery(selectQuery);
+       and to store the result we make use of the ResultSet interface.
+
+       *****************************************************************************************
+
+       Syntax for the  "SELECT" Query:
+       String selectQuery="SELECT * FROM student_table";
+       ResultSet resultSet = statement.executeQuery(selectQuery);
+       And we make use of the resultSet interface to stor the result of the query.
+       And we iterate the resultSet by using the hasnext() method.
+
+       *******************************************************************************************
+
+       Syntax for the  "INSERT" Query:
+       String insertQuery="INSERT INTO table_name (oder_of attributes) VALUES (values of attributes);
+       int Rows_affected=statement.executeUpdate(insertQuery); and it will give integer as output
+       and by using the simple if condition we can able to check how many rows are affected.
+       
+       And same Syntax for the "UPDATE" and "DELETE" Query.
+
+       *********************************************************************************************
+      */
     }
     catch(SQLException e){
       System.out.println(e.getMessage());
--- readme.md
+++ readme.md
@@ -1,1 +1,53 @@
-because of using the statement we have to create multiple variable for each operation this is the main disadvantage.
+Performing the Curd Operations Using the CreateStatements.
+/
+
+---
+
+     Step-1: Loading the Driver's by using Class.forName();
+
+    *****************************************************************************
+
+      Step-2: Creating a Connection object by using DriverManager and
+      we make use of the connection interface to store the instance of the
+      connection created by the DriverManager;
+
+     *****************************************************************************
+
+      Step-3: Creating a Statement object by using the Connection interface
+      to excute the SQL Queries.
+
+      ****************************************************************************
+
+      Step-4: Executing the SQL Queries by using the Statement interface
+       And  when we are using the createStatement() we have to write the SQL
+       Commands Manually.
+       And we have Different types of SQL Commands like
+       1. SELECT
+       2. INSERT
+       3. UPDATE
+       4. DELETE
+       To excute the "SELECT" we will make use of the statement.executeQuery(selectQuery);
+       and to store the result we make use of the ResultSet interface.
+
+       ***************************************************************************
+
+       Syntax for the  "SELECT" Query:
+       String selectQuery="SELECT * FROM student_table";
+       ResultSet resultSet = statement.executeQuery(selectQuery);
+       And we make use of the resultSet interface to stor the result of the query.
+       And we iterate the resultSet by using the hasnext() method.
+
+       ***************************************************************************
+
+       Syntax for the  "INSERT" Query:
+       String insertQuery="INSERT INTO table_name (oder_of attributes) VALUES (values of attributes);
+       int Rows_affected=statement.executeUpdate(insertQuery); and it will give integer as output
+       and by using the simple if condition we can able to check how many rows are affected.
+
+       And same Syntax for the "UPDATE" and "DELETE" Query.
+
+       ***************************************************************************
+
+---
+
+    /
